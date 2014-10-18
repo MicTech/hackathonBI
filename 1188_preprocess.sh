3dddf7121ed4a7bd63d8e0f3fc386094122fbb6a
Michal Tehnik <michal.tehnik@mictech.cz>
2014-10-18 14:55:27 +0200

    Add script for download and preprocess data

---
+++ 1188_preprocess.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+wget http://cdn.hackathon.bi/data/1188/sample_1M/ope-search/ope-search.csv
+
+echo "id;created_at;params" > ope-search_preprocess.csv
+cat ope-search.csv | sed -e 's/""/"/g' | sed "s/\"{/{/g" | sed "s/}\"/}/g" | sed "s/:null/:\"null\"/g" >> ope-search_preprocess.csv
