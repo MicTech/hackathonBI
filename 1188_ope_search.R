35c504a802a16dfa31dd87d4a6fc1d17aab27f26
Michal Tehnik <michal.tehnik@mictech.cz>
2014-10-18 14:55:55 +0200

    Improve Transformation

--- 1188_ope_search.R
+++ 1188_ope_search.R
@@ -5,18 +5,30 @@ install.packages("jsonlite", repos="http://cran.r-project.org")
 library(jsonlite)
 
 #load data set
-geo <- read.csv('1188_ope_search.csv', header=TRUE, stringsAsFactors=F, sep=";", quote="")
+geo_original <- read.csv('ope-search_preprocess.csv', header=TRUE, stringsAsFactors=F, sep=";", quote="", na.strings="")
+
+geo <- geo_original[c(0:15000),]
 
 for(i in 1:nrow(geo)) {
   params <- fromJSON(geo[i,3])
   for(j in 1:length(params)) {
     param <- params[j]
-    print(paste("i:", i, "key:", names(param), "value:", param[1]))
-    geo[i, names(param)] <- paste(param[1])
+    currentvalue <- paste(param[1])
+    
+    if(names(param) == "uir_type") {
+      key <- paste0("uir_type_", currentvalue)
+      value <- params[j+2][1]
+      geo[i, key] <- paste(value)
+    } else {
+      geo[i, names(param)] <- paste(currentvalue)  
+    }    
   }
+  
+  geo[i, "country"] <- "czech republic"
+  print(paste("row:", i))
 }
 
 #remove params column 
 geo$params <- NULL
 
-write.csv(geo, file = "1188_ope_search_transformed.csv", row.names = FALSE)
+write.csv(geo, file = "ope-search_transformed_1.csv", row.names = FALSE, quote=FALSE)
