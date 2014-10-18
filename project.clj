ed1215f8b05171f6f85fcbaba466b25edc63a2ac
Aleš Roubíček <ales@roubicek.name>
2014-10-18 19:43:15 +0200

    Added clojure script to export data

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject sandbox "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
+                 [org.clojure/data.csv "0.1.2"]
+                 [com.cognitect/transit-clj "0.8.259"]
+                 [incanter "1.5.5"]
+                 [clj-time "0.8.0"]]
+  :main hackathon-bi.core)
---
+++ src/hackathon_bi/core.clj
@@ -0,0 +1,30 @@
+(ns hackathon-bi.core
+  (:require [clojure.java.io :as io]
+            [clojure.data.csv :as csv]
+            [clj-time.core :as t]
+            [clj-time.coerce :as c]
+            [cognitect.transit :as transit]
+            [incanter.core :as i]
+            [incanter.io :as iio])
+  (:import [java.io File ByteArrayInputStream]))
+
+(defn data [uri]
+  (iio/read-dataset uri :delim \;))
+
+(defn in [x] (ByteArrayInputStream. (.getBytes x)))
+(defn reader [in] (transit/reader in :json))
+
+(defn process [data]
+  (->> data :rows
+       (map #(transit/read (reader (in (:col2 %)))))
+       #_(map (fn [x]
+              (assoc x
+                (str "uir_type_" (get x "uir_type"))
+                (get x "uir_name"))))
+       #_(map #(assoc % "country" "Czech republic"))
+       #_(map (fn [x] (select-keys x ["country" "uir_type_address" "where" "what" "source_facility" "from" "size" "uir_type_city" "uir_type_city_part" "uir_type_street" "uir_type_urban_district" "uir_type_district" "uir_type_region"])))
+       (map (fn [x] (print x) x))))
+
+(defn -main [& [in out]]
+  (with-open [out-file (io/writer out)]
+    (csv/write-csv out-file (process (data in)))))
