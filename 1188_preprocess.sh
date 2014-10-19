f7be204e68993513f901ed3f400de85399add3f9
Michal Tehnik <michal.tehnik@mictech.cz>
2014-10-19 13:43:03 +0200

    Add additional preprocess steps

--- 1188_preprocess.sh
+++ 1188_preprocess.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-wget http://cdn.hackathon.bi/data/1188/sample_1M/ope-search/ope-search.csv
+#wget http://cdn.hackathon.bi/data/1188/sample_1M/ope-search/ope-search.csv
 
 echo "id;created_at;params" > ope-search_preprocess.csv
-cat ope-search.csv | sed -e 's/""/"/g' | sed "s/\"{/{/g" | sed "s/}\"/}/g" | sed "s/:null/:\"null\"/g" >> ope-search_preprocess.csv
+cat ope-search.csv | sed -e 's/""/"/g' | sed 's/";/"/g' | sed "s/\"{/{/g" | sed "s/}\"/}/g" | sed "s/:null/:\"null\"/g" | tr "\t" " " >> ope-search_preprocess.csv
+head ope-search_preprocess.csv
